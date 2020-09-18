cd5b7b1ad2826565a79d1ddd63acb4104d85d17c
Dion Feng <55362194+DioFeng@users.noreply.github.com>
2020-09-18 09:47:38 -0700

    Create KLists.java

---
+++ KLists.java
@@ -0,0 +1,20 @@
+public class KLists
+{
+  //function
+  public double [] mergeKLists (double [][] outerArray)
+  {
+  //code
+  }
+  
+  public static void main(String[] args)
+  {
+  Integer[][] inputArray1 = {{1.1,4.4,5.5},{1.1,3.3,4.4},{2.2,6.6}};
+  //Expected output: [1.1, 1.1, 2.2, 3.3, 4.4, 4.4, 5.5, 6.6]
+  Integer[] inputArray2;
+  //Expected output: []
+  Integer[][] inputArray3 = new Integer[][]{inputArray2};
+  //Expected output: [[]]
+  Integer[][] inputArray4 = {{9.7,17.1},{15.8},{12.7, 18.5, 21.27}};
+  //Expected output: [9.7, 12.7, 15.8, 17.1, 18.5, 21.27]
+  }
+  
