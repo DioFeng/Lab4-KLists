677a9112b8a51278acf8150db08d0282a1bdda12
dionfeng <dfeng5@usfca.dons.edu>
2020-09-25 02:50:29 -0700

    update

--- KLists.java
+++ KLists.java
@@ -1,38 +1,117 @@
 import java.util.ArrayList;
+import java.util.*;
 public class KLists
 {
-  private ArrayList<ArrayList<Double>> sample = new ArrayList<ArrayList<Double>>();
-  //function
-  public KLists(double[][] input)
+  private ArrayList<Double> sample = new ArrayList<Double>();
+  private ArrayList<ArrayList<Double>> nullArr = new ArrayList<ArrayList<Double>>();
+ // private double[] nullArr = {};
+  private double[] output;
+
+
+  public double[] mergeKLists (double [][] outerArray)
   {
-    for(int i = 0; i<input.length;i++)
+  	doThis(outerArray);
+  	merge(combineList(outerArray));// return a list of all number put in(without sort)
+  	return output;
+  	
+  }
+  public boolean ifEmpty()
+  {
+  	
+  	return nullArr.isEmpty();
+  }
+  public void doThis(double[][]arr)
+  {
+  	for(int i = 0; i<arr.length;i++)
     {
-        sample.add(new ArrayList<Double>());
+        nullArr.add(new ArrayList<Double>());
     
+      for (int j = 0; j<arr[i].length;j++)
+      { 
+        nullArr.get(i).add(arr[i][j]); 
+      }
+  	}
+  }
+  public double[] combineList(double [][] input)
+  {
+  	for(int i = 0; i<input.length;i++)
+    {
       for (int j = 0; j<input[i].length;j++)
-      {
-        System.out.println(input[i][j]);
-        
-        sample.get(i).add(input[i][j]);      
-      
+      {       
+        sample.add(input[i][j]);      
       }
     }
+
+    double[] list = new double[sample.size()];
     System.out.println(sample);
+
+  	for(int h =0; h<sample.size();h++)
+  		list[h]=sample.get(h);
+
+  	return list;
   }
-  public double [] mergeKLists (double [][] outerArray)
+
+  public void merge(double[] list)
   {
-  //code
-    return null;
+  	int length = list.length;
+
+  	if(length<=1)
+  		return;
+
+  	double[] first = Arrays.copyOfRange(list,0,length/2);
+  	double[] second = Arrays.copyOfRange(list,length/2,length);
+
+  	merge(first);
+  	merge(second);
+  	mergeSortLists(first, second, list);
   }
-  public String toString()
+  
+  public void mergeSortLists(double[]first,double[]second,double[]list)
   {
-    
-    String result = "";
-    for(int i = 0; i<sample.size();i++)
-      result+=sample.get(i);
-    return result;
+  	int fir = 0;
+  	int sec = 0;
+  	int position = 0;
+  	//output = new double[list.length];
+	while(fir<first.length && sec<second.length)
+	{
+
+		if(first[fir]<second[sec])
+		{
+			list[position++]=first[fir++];
+		}
+		else
+		{
+			list[position++]=second[sec++];
+		}	
+	}
+
+	while(fir<first.length)
+	{
+		list[position++]=first[fir++];	
+	}
+
+	while(sec<second.length)
+	{
+		list[position++]=second[sec++];
+	}
+
+	output = Arrays.copyOfRange(list,0,list.length);
 
   }
+
+  public String toString()
+  {
+    if(ifEmpty())
+    {
+    	System.out.println("1");
+    }
+    String result = "[";
+    for(int i = 0; i<output.length;i++)
+      result+=output[i]+" ";
+  	result+="]";
+    return result;
+  }
+
   public static void main(String[] args)
   {
   double[][] inputArray1 = {{1.1,4.4,5.5},{1.1,3.3,4.4},{2.2,6.6}};
@@ -43,10 +122,16 @@ public class KLists
   //Expected output: [[]]
   double[][] inputArray4 = {{9.7,17.1},{15.8},{12.7, 18.5, 21.27}};
   //Expected output: [9.7, 12.7, 15.8, 17.1, 18.5, 21.27]
-  KLists kl = new KLists(inputArray1);
-  System.out.println(kl);
-  KLists kl1 = new KLists(inputArray4);
-  System.out.println(kl1);
+
+  KLists kl = new KLists();
+  kl.mergeKLists(inputArray1);
+  System.out.println("Output: "+kl.toString());
+  
+  KLists kl1 = new KLists();
+  kl1.mergeKLists(inputArray3);
+  //System.out.println("Output: "+kl1.toString());
+  // KLists kl1 = new KLists(inputArray4);
+  // System.out.println(kl1);
   //System.out.println();
   }
   
